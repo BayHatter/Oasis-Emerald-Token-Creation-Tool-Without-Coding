30ded827feb5bc4df8a7e5007b61d9b37faf58b5
Mohsin Khan <209888552+BayHatter@users.noreply.github.com>
2025-05-06 20:15:48 +0500

    Update and rename eth.js to oasis.js

--- pages/oasis.js
+++ pages/oasis.js
@@ -1,15 +1,15 @@
 import dynamic from 'next/dynamic';
 import WalletProvider from '../assets/WalletProvider';
 import TopMenu from '../assets/TopMenu';
-import Ethereum from '../assets/Ethereum';
+import OasisEmerald from '../assets/OasisEmerald';
 import Sidebar from '../assets/Sidebar';
 import Head from 'next/head';
 
-const EthereumWithNoSSR = dynamic(() => import('../assets/Ethereum'), {
+const OasisEmeraldWithNoSSR = dynamic(() => import('../assets/OasisEmerald'), {
   ssr: false,
 });
 
-export default function Eth() {
+export default function Oasis() {
   return (
     <div className="min-h-screen bg-white-100">
       <WalletProvider>
@@ -18,17 +18,17 @@ export default function Eth() {
             <TopMenu account={account} connectWallet={connectWallet} disconnectWallet={disconnectWallet} />
             
             <Head>
-              <title>Ethereum Token Creation Tool</title>
-              <meta name="description" content="Create and launch your own meme token on Ethereum" />
-              <meta property="og:title" content="Launch Your Meme Coin on Ethereum" />
-              <meta property="og:description" content="Build and deploy a meme coin instantly on Ethereum. Fast, secure, and 100% code-free." />
+              <title>Oasis Emerald Token Creation Tool</title>
+              <meta name="description" content="Create and launch your own meme token on Oasis Emerald" />
+              <meta property="og:title" content="Launch Your Meme Coin on Oasis Emerald" />
+              <meta property="og:description" content="Build and deploy a meme coin instantly on Oasis Emerald. Fast, secure, and 100% code-free." />
               <meta property="og:type" content="website" />
-              <meta property="og:url" content="/eth" />
+              <meta property="og:url" content="/oasis" />
             </Head>
 
             <div className="container mx-auto p-4 max-w-7xl flex flex-col md:flex-row gap-4">
               <div className="flex-1">
-                <EthereumWithNoSSR provider={provider} account={account} connectWallet={connectWallet} />
+                <OasisEmeraldWithNoSSR provider={provider} account={account} connectWallet={connectWallet} />
               </div>
               <div className="md:w-[300px] flex-shrink-0">
                 <Sidebar />
